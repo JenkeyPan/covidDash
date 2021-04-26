709717157f9a8a9efc06188d184cf9530ac766ee
pan <pzjoscar@163.com>
2021-04-26 22:06:30 +0800

    添加疫苗板块

---
+++ src/components/vaccine/vaccineMap.vue
@@ -0,0 +1,35 @@
+<template>
+   <el-row>
+        <el-col>
+            <div class="grid-content">
+                <china-vaccine-line-chart/>
+                <vaccine-digit/>
+            </div>
+        </el-col>
+    </el-row>
+</template>
+<script>
+import * as echarts from 'echarts'
+import 'echarts/lib/chart/map/china.js'//引入echarts中的中国地图
+import jsonp from 'jsonp'//引入jsonp
+import VaccineDigit from './VaccineDigit'
+import ChinaVaccineLineChart from './chinaVaccineLineChart'
+
+
+export default {
+  name: 'VaccineMap',
+  mounted() {
+  },
+  components:{
+    ChinaVaccineLineChart,
+    VaccineDigit,
+    
+  },
+  methods: {
+   
+  }
+}
+</script>
+<style>
+
+</style>
